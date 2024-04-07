56c34a3de6b320ffad875d6972985d4c32b6d033
elvin <ali.elvin@gmail.com>
2024-04-07 17:17:55 +0100

    Fix errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,8 @@ import { Metadata } from "next"
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
-import { SiteHeader } from "@/components/site-header"
-import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+import React from "react"
 
 export const metadata: Metadata = {
   title: {
